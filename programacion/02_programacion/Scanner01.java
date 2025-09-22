037ecabae59dc5c08439ecc096ff650ee2471687
Bezanilla Alonso <libeal201@iesluisbraille.es>
2025-09-22 08:31:42 +0200

    try

--- programacion/02_programacion/Scanner01.java
+++ programacion/02_programacion/Scanner01.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class Scanner01 {
 	public static void main(String[] args) {
 		Scanner intro = new Scanner(System.in); //Creo un objeto que recopila información de la terminal.
+		int edad;
 		
 		System.out.println("Introduce tu edad.");
-		int edad = intro.nextInt(); //El objeto creado arriba solo recopila numeros.
+		edad = intro.nextInt(); //El objeto creado arriba solo recopila numeros.
 		System.out.print("Tu edad es: " + edad);
 	}
 }
